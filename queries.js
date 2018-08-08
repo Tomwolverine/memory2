03b752f3b61eb2ab898a5aa54c281bbadf0522a6
tom lee <tdroid99@gmail.com>
2018-08-08 15:42:46 -0600

    finished queries

--- queries.js
+++ queries.js
@@ -2,13 +2,20 @@ const database = require("./database-connection");
 
 module.exports = {
     list(){
+        return database('coffee').select().orderBy('id', 'asc');
     },
     read(id){
+        return database('coffee').select().where('id', id);
     },
     create(coffee){
+        return database('coffee').insert(coffee).returning('*')
+            .then(returnedArray => returnedArray[0]);
     },
     update(id, coffee){
+        return database('coffee').update('coffee')where('id', id)
+            .returning('*').then(returnedArray => returnedArray[0]);
     },
     delete(id){
+        return database('coffee').delete().where('id', id);
     }
 };
