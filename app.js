67230b31d56626202c34dfbb1497f2c27c175811
tom lee <tdroid99@gmail.com>
2018-08-08 17:11:55 -0600

    adjusted app.js

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
+const cors = require('cors');
 const app = express();
 
 const coffees = require("./routes/coffees");
 
+app.use(cors());
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 
